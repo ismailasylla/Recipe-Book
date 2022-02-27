ed359726cc9bc7fc2b5b92fd855bfe7955c17114
ismailasylla <ismailasyllaa@gmail.com>
2022-02-27 21:12:47 +0400

    Adds (Shopping List Model & Ouputing The Lst.. 🥸)

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient [] = [
+    {
+      name: 'Orange',
+      amount: 64
+    },
+    {
+      name: 'Apple',
+      amount: 53
+    },
+    {
+      name: 'Tomato',
+      amount: 36
+    },
+    {
+      name: 'Banana',
+      amount: 10
+    }
+]
 
   constructor() { }
 
